fc6d732d8e5d84857e43e9f4104b485d51c7b1e1
Iman Kazerani <Kazeraniman@gmail.com>
2018-04-07 19:14:46 -0400

    Improving collision detection a bit by reducing the likelihood of muliple collisions being detected and also preventing a brick from being 'hit' multiple times in a small period of time to not double-count damage.

--- Scenes/Bricks/Brick.gd
+++ Scenes/Bricks/Brick.gd
@@ -2,16 +2,43 @@ extends Sprite
 
 signal brick_destroyed
 
-var health = 1
+export (int) var health
+var hittable = true
 
 func brick_collision():
-	health -= 1
-	if health <= 0:
-		emit_signal("brick_destroyed")
-		queue_free()
+	if hittable:
+		hittable = false
+		$HittableTimout.start()
+		health -= 1
+		if health <= 0:
+			emit_signal("brick_destroyed")
+			queue_free()
 
 func _on_BrickSides_area_entered(area):
+	# Turn off collisions so we don't get multi hits.
+	$BrickSides/CollisionLeft.disabled = true
+	$BrickSides/CollisionRight.disabled = true
+	$BrickSides/CollisionTimeout.start()
+	# Apply collision logic
 	brick_collision()
 
 func _on_BrickTopBottom_area_entered(area):
+		# Turn off collisions so we don't get multi hits.
+	$BrickTopBottom/CollisionTop.disabled = true
+	$BrickTopBottom/CollisionBottom.disabled = true
+	$BrickSides/CollisionTimeout.start()
+	# Apply collision logic
 	brick_collision()
+
+func _on_BrickSides_CollisionTimeout_timeout():
+	# Re-enable collision detection
+	$BrickSides/CollisionLeft.disabled = false
+	$BrickSides/CollisionRight.disabled = false
+
+func _on_BrickTopBottom_CollisionTimeout_timeout():
+	# Re-enable collision detection
+	$BrickTopBottom/CollisionTop.disabled = false
+	$BrickTopBottom/CollisionBottom.disabled = false
+
+func _on_HittableTimout_timeout():
+	hittable = true
