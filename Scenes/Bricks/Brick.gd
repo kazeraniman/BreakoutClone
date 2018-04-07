1f8dfcf92b55f67179cb31bf7c943b568f9456e8
Iman Kazerani <Kazeraniman@gmail.com>
2018-04-07 19:41:21 -0400

    Adjusting sprites to reflect brick level.

--- Scenes/Bricks/Brick.gd
+++ Scenes/Bricks/Brick.gd
@@ -5,11 +5,24 @@ signal brick_destroyed
 export (int) var health
 var hittable = true
 
+func _ready():
+	set_brick_texture_per_level()
+
+func set_brick_texture_per_level():
+	match health:
+		3:
+			set_region_rect(Rect2(208, 192, 48, 16))
+		2:
+			set_region_rect(Rect2(208, 160, 48, 16))
+		_:
+			set_region_rect(Rect2(208, 32, 48, 16))
+
 func brick_collision():
 	if hittable:
 		hittable = false
 		$HittableTimout.start()
 		health -= 1
+		set_brick_texture_per_level()
 		if health <= 0:
 			emit_signal("brick_destroyed")
 			queue_free()
