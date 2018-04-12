a6c9cb0b3c2609132e99e0f7178ae41331b15df0
Iman Kazerani <Kazeraniman@gmail.com>
2018-04-12 19:18:37 -0400

    Adding in a basic score mechanism and visual element which increases with each destroyed brick.

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -1,7 +1,17 @@
 extends Node
 
 var lives = 3
+var score = 0
+
+func _ready():
+	# Listen for the brick_destroyed signal from all the bricks
+	for brick in $Bricks.get_children():
+		brick.connect("brick_destroyed", self, "increase_score")
 
 func _on_Ball_death():
 	lives -= 1
 	$HUD.set_lives(lives)
+
+func increase_score():
+	score += 100
+	$HUD/Score.text = str(score)
